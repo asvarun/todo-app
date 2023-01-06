39181ce670bbfe599ddcbc2f12a3d255e2abdbed
arun <asvprogrammer2015@gmail.com>
2023-01-06 15:14:43 +0530

    Create basic GUI window without interactions - Day 16

---
+++ gui.py
@@ -0,0 +1,10 @@
+import functions
+import PySimpleGUI as psg
+
+label = psg.Text("Type in a To-Do")
+input_box = psg.InputText(tooltip="Enter todo")
+add_button = psg.Button("Add")
+
+window = psg.Window("My To-Do App", layout=[[label], [input_box, add_button]])
+window.read()
+window.close()
