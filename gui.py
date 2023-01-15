4370906b9aa06490cfb24a2fb5ded540365d8e86
arun <asvprogrammer2015@gmail.com>
2023-01-15 22:55:44 +0530

    Implement Complete & Exit buttons

--- gui.py
+++ gui.py
@@ -6,9 +6,13 @@ input_box = psg.InputText(tooltip="Enter todo", key="todo")
 add_button = psg.Button("Add")
 list_box = psg.Listbox(values=functions.get_todos(), key="todos", enable_events=True, size=(45, 10))
 edit_button = psg.Button("Edit")
+complete_button = psg.Button("Complete")
+exit_button = psg.Button("Exit")
+
+# layout = [[label], [input_box, add_button], [list_box, edit_button]]
 
 window = psg.Window("My To-Do App",
-                    layout=[[label], [input_box, add_button], [list_box, edit_button]],
+                    layout=[[label], [input_box, add_button], [list_box, edit_button, complete_button], [exit_button]],
                     font=("Helvetica", 14))
 
 while True:
@@ -27,15 +31,24 @@ while True:
         case "Edit":
             todo_to_edit = values["todos"][0]
             new_todo = values["todo"]
-
             todos = functions.get_todos()
             index = todos.index(todo_to_edit)
             todos[index] = new_todo
             functions.write_todos(todos)
             window["todos"].update(values=todos)
+        case "Complete":
+            todo_to_complete = values["todos"][0]
+            todos = functions.get_todos()
+            todos.remove(todo_to_complete)
+            functions.write_todos(todos)
+            window["todos"].update(values=todos)
+            window["todo"].update(value="")
+        case "Exit":
+            break
         case "todos":
             window["todo"].update(value=values["todos"][0])
         case psg.WIN_CLOSED:
-            break
+            break # exits the loop but continues with program
+            # exit() - stops the program completely
 
 window.close()
